53bae571973408d844ca8f5d776e69172a2de1be
Txaco <42583806+Txaco@users.noreply.github.com>
2018-09-11 05:42:24 +0200

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -24,7 +24,7 @@ let THE_ROCK_LIST = ( () => {
 		// Genius API data
 		geniusAPI: {
 			searchURI: 'https://api.genius.com/search?per_page=10&page=1&q=',
-			options: 		{ headers: { 'Authorization': `Bearer ${window.localStorage.getItem('geniusToken')}` } }
+			options: 		{ headers: { 'authorization': `Bearer ${window.localStorage.getItem('geniusToken')}` } }
 		},
 
 		// Fallback image URI
